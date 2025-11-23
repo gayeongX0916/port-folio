cc87bff6fc36aafa8bdec7557df7b2a70012049d
gayeongX0916 <gayeongx0916@gmail.com>
2025-11-23 17:05:36 +0900

    [feat/#3] 프로젝트 배경 추가

--- app/components/Sections/ProjectSection.tsx
+++ app/components/Sections/ProjectSection.tsx
@@ -1,19 +1,26 @@
+import GlobalNomad from "@/app/assets/GlobalNomad.png";
+import Taskify from "@/app/assets/Taskify.png";
+import OpenMind from "@/app/assets/OpenMind.png";
+
 export function ProjectSection() {
   const projects = [
     {
       id: 1,
       title: "GlobalNomad",
       description: "캘린더·지도 SDK 기반의 체험 예약 플랫폼",
+      img: GlobalNomad,
     },
     {
       id: 2,
       title: "Taskify",
       description: "커뮤니티 기반 일정·할 일 협업 도구",
+      img: Taskify,
     },
     {
       id: 3,
       title: "OpenMind",
       description: "익명 Q&A 커뮤니티 서비스",
+      img: OpenMind,
     },
   ];
 
@@ -25,10 +32,24 @@ export function ProjectSection() {
           {projects.map((project) => (
             <div
               key={project.id}
-              className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow"
+              className="relative h-[300px] rounded-xl overflow-hidden
+              shadow-md hover:shadow-xl cursor-pointer"
             >
-              <h3 className="text-xl font-semibold mb-4">{project.title}</h3>
-              <p className="text-gray-700">{project.description}</p>
+              <div
+                className="absolute inset-0 w-full bg-cover bg-center bg-no-repeat h-full"
+                style={{ backgroundImage: `url(${project.img.src})` }}
+              />
+              <div className="absolute inset-0 bg-black/40" />
+              <div
+                className="
+                  absolute bottom-4 right-4 text-right z-10 text-white
+                "
+              >
+                <h3 className="text-xl font-semibold mb-2 text-right">
+                  {project.title}
+                </h3>
+                <p className="text-right">{project.description}</p>
+              </div>
             </div>
           ))}
         </div>
